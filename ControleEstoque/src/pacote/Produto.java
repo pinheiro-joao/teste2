764d513720d68f6379ae9cea2e12b38774f54b30
João.Pinheiro <o.pinheiro@escolar.ifrn.edu.br>
2025-01-15 10:07:30 -0300

    Add files via upload

---
+++ ControleEstoque/src/pacote/Produto.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pacote;
+
+/**
+ *
+ * @author joaof
+ */
+public class Produto {
+    String nome;
+    double preco;
+    int qnt;
+    int codigo;
+
+    public Produto(String nome, double preco) {
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+    
+    
+    
+    
+}
